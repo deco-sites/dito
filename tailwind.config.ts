0e85379f1e5f36929e623008938cc76cff87954c
Mizuta <mizutaeric@gmail.com>
2023-06-12 20:41:12 -0300

    add font face Montserrat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,12 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: { 
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat']
+      },
+    },
+  },
+
 };
