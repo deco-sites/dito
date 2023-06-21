ef4a23c9c2b6f6b528f69f8605bd6cd63a27e88f
Mizuta <mizutaeric@gmail.com>
2023-06-21 17:37:09 -0300

    change testimonials content to dito pattern

--- sections/Content/Testimonials.tsx
+++ sections/Content/Testimonials.tsx
@@ -1,183 +1,80 @@
-import Icon from "$store/components/ui/Icon.tsx";
 import Image from "deco-sites/std/components/Image.tsx";
 import Header from "$store/components/ui/SectionHeader.tsx";
-import Slider from "$store/components/ui/Slider.tsx";
-import SliderJS from "$store/islands/SliderJS.tsx";
-import { useId } from "preact/hooks";
 import type { Image as ImageType } from "deco-sites/std/components/types.ts";
 
 export interface Testimonial {
-  text?: string;
   image?: {
     src?: ImageType;
     alt?: string;
   };
+  text?: string;
   user?: {
-    avatar?: ImageType;
     name?: string;
     position?: string;
-    company?: string;
+    href?: string;
   };
 }
 
 export interface Props {
   title?: string;
-  description?: string;
+  highlight?: string;
+  title2?: string;
   testimonials?: Testimonial[];
-  layout?: {
-    variation?: "Grid" | "Slider";
-    headerAlignment?: "center" | "left";
-  };
 }
 
-const DEFAULT_PROPS: Props = {
-  "title": "",
-  "description": "",
-  "testimonials": [{
-    "text":
-      "Fashion Store is my go-to online destination for all things stylish. Their vast collection of trendy clothes and accessories never disappoints. The quality is exceptional, and the prices are affordable. The website is easy to navigate, and their customer service team is friendly and responsive. I always feel like a fashionista when I shop here!",
-    "image": {
-      "src":
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/e0fcbcae-0a21-4269-9605-7ef8708e58ad",
-      "alt": "Testimonal",
-    },
-    "user": {
-      "avatar": "https://avatars.githubusercontent.com/u/117045675?s=200&v=4",
-      "name": "Robert Johnson",
-      "position": "Founder",
-      "company": "RJ Agency",
-    },
-  }, {
-    "text":
-      "I can't praise Fashion Store enough! Their commitment to staying ahead of the fashion curve is evident in their diverse and up-to-date inventory. Whether I need a casual outfit or a glamorous dress, they have it all. The shopping experience is seamless, and my orders always arrive promptly. Fashion Store is a true fashion lover's paradise!",
-    "image": {
-      "src":
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/e0fcbcae-0a21-4269-9605-7ef8708e58ad",
-      "alt": "Testimonal",
-    },
-    "user": {
-      "avatar": "https://avatars.githubusercontent.com/u/117045675?s=200&v=4",
-      "name": "Mary Bush",
-      "position": "Director",
-      "company": "MB & Co",
-    },
-  }, {
-    "text":
-      "Fashion Store has transformed my wardrobe. Their curated collection of clothing and accessories has helped me discover my personal style. The quality of their products is outstanding, and the prices are unbeatable. The website is visually appealing and easy to navigate. Fashion Store is my trusted companion for staying fashionable and feeling confident!",
-    "image": {
-      "src":
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/e0fcbcae-0a21-4269-9605-7ef8708e58ad",
-      "alt": "Testimonal",
-    },
-    "user": {
-      "avatar": "https://avatars.githubusercontent.com/u/117045675?s=200&v=4",
-      "name": "Sandra Bullock",
-      "position": "Founder",
-      "company": "Sanlock",
-    },
-  }],
-  "layout": {
-    "variation": "Grid",
-    "headerAlignment": "center",
-  },
-};
-
 const Testimonal = ({ image, text, user }: Testimonial) => (
-  <div class="flex flex-col items-center gap-9 text-center">
+  <div class="flex flex-col items-center max-w-xs">
     {image?.src && (
-      <Image
-        src={image.src}
-        alt={image?.alt}
-        width={100}
-        height={100}
-      />
+      <img src={image.src} alt={image?.alt} class="h-24 object-scale-down">
+      </img>
     )}
-    <h3 class="text-xl lg:text-2xl">
+    <h3 class="text-white leading-7 text-justify mb-auto pb-20">
       {text}
     </h3>
-    <div class="flex flex-col items-center gap-4">
-      {user?.avatar && (
-        <Image
-          src={user.avatar}
-          alt={user?.name}
-          width={60}
-          height={60}
-          class="rounded-full"
-        />
-      )}
-      <div class="flex flex-col">
-        {user?.name &&
-          (
-            <p class="text-lg">
-              {user?.name}
-            </p>
-          )}
-        {(user?.position || user?.company) &&
-          (
-            <p class="text-lg">
-              {user?.position}, {user?.company}
-            </p>
-          )}
-      </div>
+    <div class="flex flex-col self-start mb-7 mt-auto">
+      {user?.name &&
+        (
+          <p class="font-bold text-white">
+            {user?.name}
+          </p>
+        )}
+      {user?.position &&
+        (
+          <p class="text-white">
+            {user?.position}
+          </p>
+        )}
     </div>
+    <a
+      class={"btn btn-secondary hover:text-white text-lg px-11 mt-auto"}
+      href={user?.href}
+    >
+      VER CASE
+    </a>
   </div>
 );
 
 export default function Testimonials(
   props: Props,
 ) {
-  const id = useId();
-  const { title, description, testimonials, layout } = {
-    ...DEFAULT_PROPS,
+  const { title, highlight, title2, testimonials } = {
     ...props,
   };
 
   return (
-    <div class="w-full container px-4 py-8 flex flex-col gap-14 lg:gap-20 lg:py-10 lg:px-0">
-      <Header
-        title={title}
-        description={description}
-        alignment={layout?.headerAlignment || "center"}
-      />
+    <div class="bg-[#19223c]">
+      <div class="w-full container px-4 py-16 flex flex-col items-center">
+        <h2 class="text-secondaryTitle text-center text-white font-bold max-w-[980px] mb-4">
+          {title} <span class="text-main">{highlight}</span> {title2}
+          <span class="text-main">.</span>
+        </h2>
 
-      {layout?.variation === "Grid" && (
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <div class="flex w-full justify-between">
           {testimonials?.map(({ image, text, user }) => (
             <Testimonal image={image} text={text} user={user} />
           ))}
         </div>
-      )}
-
-      {layout?.variation !== "Grid" && (
-        <div
-          class="relative w-full px-8"
-          id={id}
-        >
-          <Slider class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5 w-full">
-            {testimonials?.map(({ image, text, user }, index) => (
-              <Slider.Item
-                index={index}
-                class="flex flex-col gap-4 carousel-item w-full"
-              >
-                <Testimonal image={image} text={text} user={user} />
-              </Slider.Item>
-            ))}
-          </Slider>
-          <>
-            <div class="z-10 absolute -left-2 lg:-left-8 top-1/2">
-              <Slider.PrevButton class="btn btn-circle btn-outline">
-                <Icon size={20} id="ChevronLeft" strokeWidth={3} />
-              </Slider.PrevButton>
-            </div>
-            <div class="z-10 absolute -right-2 lg:-right-8 top-1/2">
-              <Slider.NextButton class="btn btn-circle btn-outline">
-                <Icon size={20} id="ChevronRight" strokeWidth={3} />
-              </Slider.NextButton>
-            </div>
-          </>
-          <SliderJS rootId={id} />
-        </div>
-      )}
+      </div>
     </div>
   );
 }
